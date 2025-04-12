0607188ba5ed7ad2c1a997577935aca1c28d1790
Janpstrunn <janpstrunn@disroot.org>
2025-04-12 11:33:08 -0300

    feat: Show localhost ip address

--- run.sh
+++ run.sh
@@ -1,4 +1,13 @@
 #!/bin/env bash
 
+active_device=$(nmcli -t -f DEVICE,STATE device status |
+  grep -w "connected" |
+  grep -v -E "^(dummy|lo:)" |
+  awk -F: '{print $1}')
+output=$(nmcli -e no -g ip4.address,ip4.gateway,general.hwaddr device show "$active_device")
+
+ip_address=$(echo "$output" | sed -n '1p' | awk -F '/' '{print $1}')
+echo " * Running on http://$ip_address:5000"
+
 export FLASK_APP=app.py
 flask run --host=0.0.0.0 --port=5000
